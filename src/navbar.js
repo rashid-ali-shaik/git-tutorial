516493ffa29c459b2223cd8bc4a8fca9a070e7db
Shaik, Baji Rasheed Ali (Contractor) <BajiRasheedAli_Shaik@comcast.com>
2023-11-29 11:43:31 +0530

    added functionality

--- src/navbar.js
+++ src/navbar.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function navbar() {
-  return <div>navbar</div>;
+  return (
+    <div>
+      navbar
+      <a>hello there</a>
+    </div>
+  );
 }
 
 export default navbar;
